cf8899c115d03434380dbecdb8375178ead5e1c1
twinnayottama <twinnayotama@gmail.com>
2024-10-05 14:19:25 +0700

    Modifikasi logo untuk user admin

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -6,6 +6,8 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Login</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo.png') }}">
+
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
